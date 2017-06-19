c9d9f7253db240e14a83e74b6464411004fbce71
Andrew Cresswell <andy@pixelbin.com>
2017-06-19 17:58:13 +0100

    Interface Completed

--- app/index.js
+++ app/index.js
@@ -1,17 +1,24 @@
   // app services
 const reddcoin = require('./services/reddcoin.js');
 const wallet = reddcoin.getWalletInstance();
-window.wallet = wallet;
 
 // Angular Services Register
 const localStorage = require('./services/ls.js');
+
+// angular directives
+require('./directives/ngClickCopy.js');
+
+
+// angular bootstrap
+const modal = require('angular-ui-bootstrap/src/modal/index-nocss.js');
+
+// set wallet
+window.wallet = wallet; // access it anywhere
+browserWallet = angular.module('browserWallet', [modal, 'ngClickCopy']);
 browserWallet.service('LocalStorageService', localStorage);
 
-// directives
-
-
 // angular
-browserWallet.controller('addresses', function($scope) {
+browserWallet.controller('addresses', function($scope, $uibModal, $timeout) {
 
   $scope.start = {
     bip39seed: 'victory pilot network forward trend cup glass grape weird license melody shy',//'victory pilot network forward trend cup glass grape weird license melody shy',
@@ -27,6 +34,8 @@ browserWallet.controller('addresses', function($scope) {
     unconfirmed: 0
   };
 
+  $scope.depositAddress = '';
+
   // addresses
   $scope.addresses = {};
 
@@ -34,13 +43,26 @@ browserWallet.controller('addresses', function($scope) {
   $scope.transactions = [];
 
   // outgoing payment data
-  $scope.payment = {};
+  $scope.payment = {
+    sendTo: '',
+    amount: '',
+    password: '',
+    error: false
+  };
 
   // show seed form
   $scope.showForm = true;
 
-  // show
+  // data is loading
   $scope.isLoading = false;
+
+  // usd exchange rate
+  $scope.exchangeRate = 0.1;
+
+  // misc data - will clean stuff into here
+  $scope.misc = {
+    copied: false
+  };
 
   /*
     Wallet Functions
@@ -55,16 +77,88 @@ browserWallet.controller('addresses', function($scope) {
     $scope.start.bip39seed = wallet.getNewSeed();
   }
 
+  /*
+    Balances are returned with - attached
+    detects if minus value, makes it an abs,
+    reforms after
+   */
   $scope.formatBalance = function(num){
-    return bitcore.util.formatValue( angular.copy(num) );
+
+    let isNegative = false;
+
+    if(num < 0){
+      isNegative = true;
+      num = Math.abs(num);
+    }
+    num = bitcore.util.formatValue( angular.copy(num) );
+
+    return isNegative ? ('-' + num) : num ;
   }
 
   $scope.submitPayment = function( addressToSendFrom ){
-    $scope.isLoading = true;
-    reddcoin.sendPayment($scope.payment.sendTo, $scope.payment.amount, addressToSendFrom, $scope.payment.password, $scope.bip39seed, function(){
+
+    let error = false;
+
+    $scope.isLoading = true; // were loading
+    $scope.payment.error = false; // reset any errors
+
+    try {
+      reddcoin.sendPayment($scope.payment.sendTo, $scope.payment.amount, addressToSendFrom, $scope.payment.password, $scope.bip39seed, function(){
+        // blank cb, but functionality exists ;)
+      });
+     } catch(err){
+        if(err){
+          error = true;
+          $scope.payment.error = 'Unable to send payment, make sure your password is correct';
+        }
+     }
+
+     if(!error){
       electrum.Mediator.event.emit('transactionAdded');
-      $scope.payment = {}; // clear payment object
+      $scope.payment = {}; // clear payment object to avoid leaks
+     }
+  }
+
+  $scope.openPaymentForm = function(size, parentSelector){
+
+    var parentElem = parentSelector ?
+      angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+
+    var modalInstance = $uibModal.open({
+      animation: true,
+      ariaLabelledBy: 'modal-title',
+      ariaDescribedBy: 'modal-body',
+      templateUrl: 'sendPaymentModal.html',
+      controller: 'addresses',
+      size: size
     });
+
+  }
+
+  $scope.formatTransactionDate = function(unixtime){
+    var newDate = new Date();
+    newDate.setTime(unixtime * 1000);
+
+    return newDate.toUTCString();
+  }
+
+  $scope.getEstUsdValue = function(){
+    return (bitcore.util.formatValue($scope.account.confirmed) * $scope.exchangeRate).toFixed(7) + ' USD';
+  }
+
+  $scope.enableSendButton = function(){
+
+    let address = typeof $scope.payment !== 'undefined' && $scope.payment.sendTo.length > 0  ? true : false;
+    let amount = typeof $scope.payment !== 'undefined' && $scope.payment.amount.length > 0 ? true : false;
+    let password = typeof $scope.payment !== 'undefined' && $scope.payment.password.length > 0 ? true : false;
+
+    return address && amount && password ? false : true;
+
+  }
+
+  // copies input contents to clipboard
+  $scope.copyToClipBoard = function(){
+
   }
 
   /*
@@ -90,6 +184,7 @@ browserWallet.controller('addresses', function($scope) {
       $scope.addresses[ data.address ] = {}
     }
 
+    $scope.depositAddress = wallet.getAddresses()[0];
     $scope.addresses[ data.address ] = data;
     $scope.showForm = false;
     $scope.$evalAsync();
@@ -101,20 +196,12 @@ browserWallet.controller('addresses', function($scope) {
     $scope.transactions = wallet.getTransactions();
     $scope.account = wallet.getAccountInfo()[0];
     $scope.tipjar = wallet.getTipJar();
-
-    console.log($scope.account);
-    console.log(wallet.getTipJar());
-
     $scope.$evalAsync();
   });
 
   electrum.Mediator.event.on('idle', function(data){
     $scope.isLoading = false;
     $scope.$evalAsync();
-
-
-    //console.log(wallet);
-
   });
 
   // anything in from requests here
@@ -126,9 +213,26 @@ browserWallet.controller('addresses', function($scope) {
     }
   });
 
+  // cross controller comms
+  $scope.$on('usdUpdate', function(event, data){
+    $scope.exchangeRate = data;
+  });
+
+  // handles address copy event - emited from directive
+  $scope.$on('copied', function(){
+    $scope.misc.copied = true;
+
+    $timeout(function(){
+      $scope.misc.copied = false;
+    }, 2500);
+    $scope.$evalAsync();
+  });
+
 });
 
-// controllers
+/*
+  Handles the ticker
+ */
 browserWallet.controller('ticker', function($scope, $http, $interval, LocalStorageService) {
 
   // reddcoin ticker api url
@@ -147,6 +251,8 @@ browserWallet.controller('ticker', function($scope, $http, $interval, LocalStora
       $scope.marketData.info = JSON.parse( LocalStorageService.get('rddTickerData') );
       $scope.marketData.loading = false;
 
+      $scope.$emit('usdUpdate', $scope.marketData.info.price_usd ); // sends exchange rate to master controller above
+
     } else {
 
       $http.get($scope.marketDataUrl).then(function(response) {
@@ -154,6 +260,7 @@ browserWallet.controller('ticker', function($scope, $http, $interval, LocalStora
         LocalStorageService.set('rddTickerData', data);
         $scope.marketData = JSON.parse(data);
         $scope.marketData.loading = false;
+        $scope.$emit('usdUpdate', $scope.marketData.info.price_usd );  // sends exchange rate to master controller above
       });
 
     }
@@ -170,6 +277,7 @@ browserWallet.controller('ticker', function($scope, $http, $interval, LocalStora
         let data = JSON.stringify(response.data[0]);
         LocalStorageService.set('rddTickerData', data);
         $scope.marketData.info = JSON.parse( data);
+        $scope.$emit('usdUpdate', $scope.marketData.info.price_usd );  // sends exchange rate to master controller above
         $scope.marketData.loading = false;
       });
     }, 10000);
